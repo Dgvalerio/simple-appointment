6e876a36274f106edfe1ee793d35c1675ead2d88
davi.valerio <al.davigvalerio@gmail.com>
2024-06-15 17:06:18 -0300

    feat(header): add header component

---
+++ src/components/header/header.tsx
@@ -0,0 +1,20 @@
+import { FC } from 'react';
+
+import Link from 'next/link';
+
+import { ThemeToggleButton } from '@/components/theme-provider/theme-toggle-button';
+import { routes } from '@/utils/constants/routes';
+
+export const Header: FC = () => (
+  <header className="flex items-center justify-between gap-4 px-4 py-2 shadow-lg">
+    <div className="flex items-center gap-2">
+      <Link
+        href={routes.home}
+        className="font-black uppercase italic text-zinc-100 text-opacity-80 transition hover:text-opacity-100"
+      >
+        Apontamento Simples
+      </Link>
+    </div>
+    <ThemeToggleButton />
+  </header>
+);
