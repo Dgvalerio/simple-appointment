36fb6464d589a1569da654d268ede2325741ac35
davi.valerio <al.davigvalerio@gmail.com>
2024-06-24 11:38:15 -0300

    feat(appointment): send to firestore and add loading to components

--- src/app/(private)/(home)/components/create-form/create-form.tsx
+++ src/app/(private)/(home)/components/create-form/create-form.tsx
@@ -1,64 +1,111 @@
 'use client';
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
 
+import { useSession } from 'next-auth/react';
+
+import { addDoc, collection } from '@firebase/firestore';
 import { zodResolver } from '@hookform/resolvers/zod';
 
 import { appointmentSchema } from '@/app/(private)/(home)/components/create-form/schema';
 import { Form } from '@/components/form/form';
 import { Button } from '@/components/ui/button';
+import { db } from '@/lib/firebase/config';
 
 import { format } from 'date-fns';
+import { toast } from 'sonner';
 import { z } from 'zod';
 
 const getToday = (): string => format(new Date(), 'yyyy-MM-dd');
 
-type Appointment = z.infer<typeof appointmentSchema>;
+type CreateAppointment = z.infer<typeof appointmentSchema>;
+
+const AppointmentController = {
+  async create(data: CreateAppointment & { userId?: string }): Promise<void> {
+    if (!data.userId) {
+      return void toast.warning('Nenhum usuário foi informado!');
+    }
+
+    await addDoc(collection(db, 'appointment'), {
+      day: data.day,
+      startTime: data.startTime,
+      endTime: data.endTime,
+      description: data.description,
+      userId: data.userId,
+    });
+
+    toast.success(
+      `Apontamento criado para o dia "${data.day} as ${data.startTime}:${data.endTime}"`
+    );
+  },
+};
 
 export const AppointmentCreateForm: FC = () => {
-  const form = useForm<Appointment>({
+  const { data: sessionData } = useSession();
+  const form = useForm<CreateAppointment>({
     resolver: zodResolver(appointmentSchema),
   });
 
+  const [loading, setLoading] = useState(false);
+
   const clearHandler = (): void => form.reset();
 
-  const submitHandler: SubmitHandler<Appointment> = async (data) => {
-    console.log('AppointmentCreateForm > submitHandler:', data);
+  const submitHandler: SubmitHandler<CreateAppointment> = async (formData) => {
+    setLoading(true);
+
+    await AppointmentController.create({
+      ...formData,
+      userId: sessionData?.id,
+    });
+
+    setLoading(false);
   };
 
   return (
-    <Form.Root<Appointment>
+    <Form.Root<CreateAppointment>
       {...form}
       onSubmit={submitHandler}
       className="flex flex-col gap-2"
     >
       <div className="flex gap-2">
-        <Form.Input<Appointment>
+        <Form.Input<CreateAppointment>
+          loading={loading}
           label="Dia"
           name="day"
           containerClassName="flex-1"
           type="date"
           defaultValue={getToday()}
         />
-        <Form.Input<Appointment>
+        <Form.Input<CreateAppointment>
+          loading={loading}
           label="Hora inicial"
           name="startTime"
           containerClassName="flex-1"
           type="time"
         />
-        <Form.Input<Appointment>
+        <Form.Input<CreateAppointment>
+          loading={loading}
           label="Hora final"
           name="endTime"
           containerClassName="flex-1"
           type="time"
         />
       </div>
-      <Form.Textarea<Appointment> label="Descrição" name="description" />
+      <Form.Textarea<CreateAppointment>
+        loading={loading}
+        label="Descrição"
+        name="description"
+      />
       <div className="mt-2 flex justify-between gap-2">
-        <Button className="w-[25%]" variant="outline" onClick={clearHandler}>
+        <Button
+          loading={loading}
+          className="w-[25%]"
+          variant="outline"
+          onClick={clearHandler}
+        >
           Limpar
         </Button>
-        <Button className="w-[25%]" type="submit">
+        <Button loading={loading} className="w-[25%]" type="submit">
           Criar
         </Button>
       </div>
